bc0deb8d8ac4e24cdb527884de55f7042a2facf9
manish-dalal <md4640@gmail.com>
2020-02-24 01:28:54 +0530

    Updates

---
+++ precache-manifest.7801734160f9ba4e34a49dc66daea6cd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a00c656ff9d79b484dc049041dfe7d6b",
+    "url": "/greytip_frontend/index.html"
+  },
+  {
+    "revision": "ac6fc26c1919224fa3b0",
+    "url": "/greytip_frontend/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d62e58c006d0ffa35b38",
+    "url": "/greytip_frontend/static/css/main.7bb4a689.chunk.css"
+  },
+  {
+    "revision": "ac6fc26c1919224fa3b0",
+    "url": "/greytip_frontend/static/js/2.a70e715a.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/greytip_frontend/static/js/2.a70e715a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d62e58c006d0ffa35b38",
+    "url": "/greytip_frontend/static/js/main.28c57994.chunk.js"
+  },
+  {
+    "revision": "8a84b5827992f7a95fa3",
+    "url": "/greytip_frontend/static/js/runtime-main.a75e3443.js"
+  },
+  {
+    "revision": "d4cee3db6f9b387e9087643692617856",
+    "url": "/greytip_frontend/static/media/defaultBg.d4cee3db.jpg"
+  },
+  {
+    "revision": "82a318ccea22a8af9fc4590f41df9238",
+    "url": "/greytip_frontend/static/media/logo.82a318cc.png"
+  }
+]);
